8ec1842a7b7eac569c791d85df27d47e10f0e7cf
AdepthShar <dieren1234@hotmail.com>
2022-03-29 22:26:51 +0200

    werkende commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 window.addEventListener('load', init);
 
 //Globals
-let apiUrl = 'http://localhost/prog3-magazine/webservice/';
+let apiUrl = 'http://localhost/prog3-magazine/webservice/';let cardData = {};
 let infoGallery;
+let detailModal;
+let detailModalContent;
+let detailModalCloseButton;
 
 function init() {
     infoGallery = document.getElementById('info-gallery')
-    // getInfoData(apiUrl, createInfoCards);
     getInfoData()
 }
-//function getInfoData(url, successHandler) {
+
 function getInfoData() {
-    //fetch(url)
     fetch(apiUrl)
         .then((response) => {
             if(!response.ok) {
@@ -19,7 +20,6 @@ function getInfoData() {
             }
             return response.json();
         })
-        //.then(successHandler)
         .then(createInfoCards)
         .catch(ajaxErrorHandler)
 }
@@ -29,25 +29,13 @@ function getInfoData() {
  *
  * @param data
  */
-// line15: results is fout, wat voor woord gebruik je om data op te halen?
 function createInfoCards(data) {
-    // console.log(data)
     for (let card of data) {
         let infoCard = document.createElement('div');
         infoCard.classList.add('dish');
         infoCard.dataset.name = card.name;
 
         infoGallery.appendChild(infoCard);
-
-        // fetch(apiUrl)
-        //     .then((response) => {
-        //         if (!response.ok) {
-        //             throw new Error(response.statusText);
-        //         }
-        //         return response.json(); })
-        //     .then(fillInfoCard)
-        //     .catch(ajaxErrorHandler);
-        // let infoCard = document.querySelector(`.dish[data-name='${.name}']`);
 
         // Print name
         let title = document.createElement('h2');
@@ -58,28 +46,45 @@ function createInfoCards(data) {
         let image = document.createElement('img');
         image.src = `${card.image}`;
         infoCard.appendChild(image);
+
+        // Details button
+        let button1 = document.createElement('button');
+        button1.innerHTML = "Details";
+        button1.dataset.name = card.name;
+        button1.dataset.id = card.id;
+        button1.classList.add('button');
+        infoCard.appendChild(button1);
+
+        // Favorite button
+        let button2 = document.createElement('button');
+        button2.innerHTML = "Favoriet";
+        button2.dataset.id = card.id;
+        button2.classList.add('button-favo');
+        infoCard.appendChild(button2);
+
+        cardData[card.id] = card;
+
     }
-console.log(data);
 }
 
-/**
- * Create the actual contents of the card based on the loaded API information
- *
- * @param data
- */
-function fillInfoCard(data) {
-    let infoCard = document.querySelector(`.dish[data-name='${data.name}']`);
-
-    // Print name
-    let title = document.createElement('h2');
-    title.innerHTML = `${data.name}`;
-    infoCard.appendChild(title);
-
-    // Print img
-    let image = document.createElement('img');
-    image.src = `${data.image}`;
-    infoCard.appendChild(image);
-}
+// /**
+//  * Create the actual contents of the card based on the loaded API information
+//  *
+//  * @param data
+//  */
+// function fillInfoCard(data) {
+//     let infoCard = document.querySelector(`.dish[data-name='${data.name}']`);
+//
+//     // Print name
+//     let title = document.createElement('h2');
+//     title.innerHTML = `${data.name}`;
+//     infoCard.appendChild(title);
+//
+//     // Print img
+//     let image = document.createElement('img');
+//     image.src = `${data.image}`;
+//     infoCard.appendChild(image);
+// }
 
 /**
  * Show an error message to inform the API isn't working correctly
