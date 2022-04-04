a974e8459b5c45d09c6fda98cb1542df1ffd2645
AdepthShar <dieren1234@hotmail.com>
2022-04-05 00:05:27 +0200

    werkende commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 window.addEventListener('load', init);
 
 //Globals
-let apiUrl = 'http://localhost/prog3-magazine/webservice/';let cardData = {};
-let infoGallery;
-let detailModal;
-let detailModalContent;
-let detailModalCloseButton;
+let apiUrl = 'http://localhost/prog3-magazine/webservice/';
+let cardData = {};
+let favoriteItems = {};
+
+let infoGallery = document.getElementById('info-gallery');
+let detailModal = document.getElementById('detail-gallery');
+let favoButton = document.getElementById('faveButton');
 
 function init() {
-    infoGallery = document.getElementById('info-gallery')
     getInfoData()
+    setTimeout(loadFavorites, 1000);
+
+    infoGallery.addEventListener('click', galleryClickHandler);
+    favoButton.addEventListener('click', addToFavorite)
+    // infoGallery.addEventListener('click', addToFavorite);
 }
 
 function getInfoData() {
@@ -26,7 +32,6 @@ function getInfoData() {
 
 /**
  * Create initial Pokémon cards based on initial API data
- *
  * @param data
  */
 function createInfoCards(data) {
@@ -52,43 +57,80 @@ function createInfoCards(data) {
         button1.innerHTML = "Details";
         button1.dataset.name = card.name;
         button1.dataset.id = card.id;
-        button1.classList.add('button');
         infoCard.appendChild(button1);
 
         // Favorite button
         let button2 = document.createElement('button');
-        button2.innerHTML = "Favoriet";
+        button2.innerHTML = '<i class="fa-regular fa-heart"></i>';
         button2.dataset.id = card.id;
-        button2.classList.add('button-favo');
+        button2.classList.add('favo');
+        button2.id = "faveButton";
         infoCard.appendChild(button2);
 
         cardData[card.id] = card;
-
     }
 }
 
-// /**
-//  * Create the actual contents of the card based on the loaded API information
-//  *
-//  * @param data
-//  */
-// function fillInfoCard(data) {
-//     let infoCard = document.querySelector(`.dish[data-name='${data.name}']`);
-//
-//     // Print name
-//     let title = document.createElement('h2');
-//     title.innerHTML = `${data.name}`;
-//     infoCard.appendChild(title);
-//
-//     // Print img
-//     let image = document.createElement('img');
-//     image.src = `${data.image}`;
-//     infoCard.appendChild(image);
-// }
+function galleryClickHandler(e) {
+    let clickedItem = e.target;
+    let descriptionInfo = "http://localhost/prog3-magazine/webservice/index.php?id=" + e.target.dataset.id;
+    console.log(descriptionInfo);
+
+    //Check if we clicked on a button
+    if (clickedItem.nodeName !== 'BUTTON') {
+        return;
+    }
+
+    //Get the information from the global stored data
+    let card = cardData[clickedItem.dataset.id];
+
+    //Reset the content
+    detailModal.innerHTML = '';
+
+    //Show the name we used on the main grid
+    let title = document.createElement('h1');
+    title.innerHTML = `${card.name} (#${card.id})`;
+    detailModal.appendChild(title);
+
+    //Display the shiny
+    let image = document.createElement('img');
+    image.src = `${card.image}`;
+    detailModal.appendChild(image);
+
+    //TODO: Print de data uit de tags in de webservice
+    let dataInfo = document.createElement('p');
+    dataInfo.innerHTML = `${e.recipe}`;
+    detailModal.appendChild(dataInfo);
+}
+
+function addToFavorite(value) {
+    favoriteItems.push(value);
+    localStorage.setItem('todoItems', JSON.stringify(favoriteItems));
+    //TODO: Verander leeg hartje in vol hartje met CSS.
+    //TODO: Verander dish class naar favoItem.
+}
+
+function removeFavorite() {
+    detailModal.classList.remove('hearth');
+}
+
+function loadFavoritesCss(favoItem) {
+
+}
+
+function loadFavorites() {
+    //TODO: Haal alle favorieten uit localstorage en print ze.
+    let favoriteItems = localStorage.getItem('todoItems');
+    if (favoriteItems !== null) {
+        favoItems = JSON.parse(favoriteItems);
+        for (let favoItem of favoItems) {
+            loadFavoritesCss(favoItem);
+        }
+    }
+}
 
 /**
  * Show an error message to inform the API isn't working correctly
- *
  * @param data
  */
 function ajaxErrorHandler(data) {
